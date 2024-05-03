d33609d9ee0979ef262ad91e86b20f32d002a826
Abhishek suman <abhisheksumancode@gmail.com>
2024-05-03 08:20:38 +0530

    skill section added - was super expensive timewise

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 "use client"
-import { ModeToggle } from "@/components/utils/ThemeSetter"
 import * as React from "react"
-import Image from "next/image";
-
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+import { HoverEffect } from "@/components/ui/card-hover-effect";
 
 export default function Home() {
 
 
   return (
 
-    <main className="flex  flex-col items-center justify-between p-16 font-light text-[#bbbbbb] ">
+    <main className="flex  flex-col items-center justify-between font-light text-[#bbbbbb] ">
 
-      <nav className="text-center fixed z-3  rounded-3xl w-auto p-4 border bg-[#72727233] bgblur ">
+      <nav className="text-center fixed z-3  rounded-3xl mt-[4%] w-auto p-4 border bg-[#72727233] bgblur ">
         <ul className="flex justify-center space-x-6 md:space-x-20  ">
           <li><a href="#" className="hover:text-white duration-300">Home</a></li>
           <li><a href="#" className="hover:text-white duration-300">About</a></li>
@@ -21,23 +20,30 @@ export default function Home() {
         </ul>
       </nav>
 
-      <section className="flex items-center gap-1 justify-center h-[80vh] flex-col w-[90vw] ">
+      <section className="flex items-center gap-1 justify-center h-[90vh] flex-col w-[98vw] bg-no-repeat bg-[url('/gradient-625d94e6.webp')]">
 
         <h1 className="text-3xl  md:text-7xl"></h1>
-        <span className=" md:text-6xl text-center text-white">Hi. I'm Abhishek.</span>
-        <span className="md:text-6xl p-2 text-center bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] animate-gradient ">A Digital Crafter-Crafting digital elegance</span>
-        <span className="text-sm text-center md:text-xl m-2">Passionate about crafting user-centric experiences intuitive, accessible,and tailored to user needs.</span>
+        <TextGenerateEffect words="Hi. I'm Abhishek." />
 
+        <span className="md:text-6xl p-2 text-center bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] animate-gradient ">A Digital Crafter-Crafting digital elegance
+        </span>
+
+        <span className="text-sm text-center md:text-xl m-2">Passionate about crafting user-centric experiences intuitive, accessible,and tailored to user needs.</span>
       </section>
 
       <div className="p-8 ">
 
-        <section className="text-justify ">
+        <section className="text-justify  w-[82vw] ">
           <h1 className="text-white text-2xl">About</h1>
-          <p className="text-sm md:text-base mt-2 w-[90vw] ">
-            I'm a 23 year old developer living in India. I discovered my passion for coding after seeing movies. I fell in love with using my creativity to build things for the web. Through persistence, self-discipline, and commitment, I achieved my goal of becoming a frontend developer. What kept me on this journey is that I always find learning new things exciting and facing unfamiliar challenges. In addition to coding, I enjoy building side projects because it allows me to explore my creativity and accomplish exciting things.
+          <p className="text-sm md:text-base mt-2">
+            I'm a 23 year old developer living in India. I discovered my passion for coding after watching movies back in 2012 but i started coding at 19. I fell in love with using my creativity to build things for the web.What kept me on this journey is that I always find learning new things exciting and facing unfamiliar challenges. In addition to coding, I enjoy building side projects because it allows me to explore my creativity and accomplish exciting things.Right now i am looking for junior/associates role where i can contribute and grow.
           </p>
         </section>
+
+        <div className="">
+          <HoverEffect items={projects} className=""/>
+        </div>
+
 
       </div>
 
@@ -47,3 +53,62 @@ export default function Home() {
     </main>
   );
 }
+
+export const projects = [
+  {
+    title: "HTML",
+    link: "/html.png",
+  },
+  {
+    title: "CSS",
+    link: "/text.png",
+  },
+  {
+    title: "Javascript",
+    link: "js.png",
+  },
+  {
+    title: "Typescript",
+    link: "typescript.png",
+  },
+  {
+    title: "React",
+    link: "atom.png",
+  },
+  {
+    title: "NextJS",
+    link: "next.png",
+  },
+  {
+    title: "Redux",
+    link: "redux.png",
+  },
+  {
+    title: "Tailwind",
+    link: "tailwind.png",
+  },
+  {
+    title: "MongoDB",
+    link: "mongodb.png",
+  },
+  {
+    title: "SQL",
+    link: "sql.png",
+  },
+  {
+    title: "Github",
+    link: "github.png",
+  },
+  {
+    title: "Linux",
+    link: "linux.png",
+  },
+  {
+    title: "Insomania",
+    link: "insomnia.png",
+  },
+  {
+    title: "Postman",
+    link: "postman.png",
+  },
+];
---
+++ src/components/ui/card-hover-effect.tsx
@@ -0,0 +1,93 @@
+import { cn } from "../utils/cn";
+import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+export const HoverEffect = ({
+  items,
+  className,
+}: {
+  items: {
+    title: string;
+    link: string;
+  }[];
+  className?: string;
+}) => {
+  let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-3 md:grid-cols-4  lg:grid-cols-7  py-6",
+        className
+      )}
+    >
+      {items.map((item, idx) => (
+        <div
+          key={item?.link}
+          className="relative group  block p-2 h-full w-full"
+          onMouseEnter={() => setHoveredIndex(idx)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
+          <AnimatePresence>
+            {hoveredIndex === idx && (
+              <motion.span
+                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block"
+                layoutId="hoverBackground"
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: { duration: 0.15 },
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 0.15, delay: 0.2 },
+                }}
+              />
+            )}
+          </AnimatePresence>
+          <Card className="p-[1px] ">
+            <img src={item.link} alt=""  className="h-6 w-6 md:h-8 md:w-8" />
+            <CardTitle className=" font-light  text-[8px] md:text-base">{item.title}</CardTitle>
+          </Card>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const Card = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        " h-full w-full overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+        className
+      )}
+    >
+      <div className="relative z-50 ">
+        <div className="p-2 flex items-center justify-center gap-4   ">{children}</div>
+      </div>
+    </div>
+  );
+};
+export const CardTitle = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <h4 className={cn("text-zinc-100 tracking-wide", className)}>
+      {children}
+    </h4>
+  );
+};
+
