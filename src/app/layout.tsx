ac5342939d8a884bb13a04be046254e415540b57
Abhishek suman <abhisheksumancode@gmail.com>
2024-05-07 11:39:36 +0530

    vercel speed insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
-import Head from 'next/head';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -21,7 +21,7 @@ export default function RootLayout({
   <link rel="icon" href="/favicon.ico" sizes="any" />
   return (
     <html lang="en">
-      
+      <SpeedInsights/>
       <body className={inter.className}><ThemeProvider
         attribute="class"
         defaultTheme="system"
